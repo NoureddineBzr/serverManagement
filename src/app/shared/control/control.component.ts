044b90a188014f1e45c09660ac819332cfc759d0
unknown <bouzire.noureddine1@gmail.com>
2025-02-04 09:05:46 +0100

    Accessing host elements programmatically

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -6,8 +6,15 @@ import { Component, input, Input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: { 
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
+  onClick(){
+    console.log('Button Clicked');
+  }
 }
