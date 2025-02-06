bc0d2f6cc9f80b1a04c25b6d2223a750dcabab75
unknown <bouzire.noureddine1@gmail.com>
2025-02-06 13:21:17 +0100

    addig the components hooks

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,7 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-currentStatus: any;
+export class ServerStatusComponent implements OnInit, OnDestroy {
+  currentStatus : 'online' | 'offline' | 'unknown' = 'offline';
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+      if(rnd > 0.5) {
+        this.currentStatus = 'online';
+      } else if(rnd < 0.9){
+        this.currentStatus = 'offline';
+      }
+       else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    
+  }
   
 }
